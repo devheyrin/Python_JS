b363e534816fe6ab27ba2631d05c812b0a712e08
devheyrin <beforerin@gmail.com>
2021-06-08 14:54:15 +0900

    Second Commit

--- js/urls.py
+++ js/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('js01', TemplateView.as_view(template_name='js01.html'), name='home'),
     path('js02', TemplateView.as_view(template_name='js02.html'), name='home'),
+path('js03', TemplateView.as_view(template_name='js03.html'), name='home'),
+path('js04', TemplateView.as_view(template_name='js04.html'), name='home'),
+path('js05', TemplateView.as_view(template_name='js05.html'), name='home'),
 ]
