8a2133ff53b9de841f6f9842d7e1596dd357cba1
devheyrin <beforerin@gmail.com>
2021-06-08 14:37:31 +0900

    Initial commit

---
+++ static/js/js01.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+            var a = 100;
+            var b = 200;
+            if(a < b){
+                a += 1000;
+            };
+                document.querySelector('#hh1').innerHTML = 'JavaScript';
+            };
