5530c7c45ec4cbfb22760e2aaf1eda68995e9fcf
devheyrin <beforerin@gmail.com>
2021-06-09 17:57:14 +0900

    Third Commit

--- js/views.py
+++ js/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def loginimpl(request):
+    id = request.POST['id'];
+    pwd = request.POST['pwd'];
+    print(id,pwd);
+    return render(request, 'jq04.html')
+
+def registerimpl(request):
+    id = request.GET['id'];
+    pwd = request.GET['pwd'];
+    name = request.GET['name'];
+    print(id, pwd, name);
+    context = {
+        'name' : name
+    }
+    return render(request, 'jq05.html',context)
